7bb7874baffe376719b0311104723276885cc1e1
Zebreus <zebreus@zebre.us>
2026-02-04 17:18:04 +0100

    Move wasixcc env vars to the main build script

--- wasix-full.sh
+++ wasix-full.sh
@@ -10,6 +10,15 @@ export WASIX_PKG_CONFIG_SYSROOT_DIR="$WASIXCC_SYSROOT"
 export WASIX_PKG_CONFIG_LIBDIR="$WASIXCC_SYSROOT/usr/local/lib/wasm32-wasi/pkgconfig"
 export WASM_RUNTIME=wasmer
 
+export WASIXCC_RUN_WASM_OPT="no"
+export WASIXCC_WASM_EXCEPTIONS="yes"
+export WASIXCC_PIC="yes"
+export WASIXCC_INCLUDE_CPP_SYMBOLS="yes"
+# Add the other locations to the search path
+export WASIXCC_COMPILER_FLAGS="-Wl,-L${WASIXCC_SYSROOT}/usr/local/lib/wasm32-wasi:-I${WASIXCC_SYSROOT}/usr/local/include:-Wl,-mllvm,--wasm-enable-eh:-Wl,-mllvm,--wasm-enable-sjlj:-Wl,-mllvm,--wasm-use-legacy-eh=false:-Wl,-mllvm,--exception-model=wasm:-iwithsysroot:/usr/local/include/c++/v1"
+# Make clang emit the non-legacy exception handling directly
+export WASIXCC_LINKER_FLAGS="-mllvm:--wasm-enable-eh:-mllvm:--wasm-enable-sjlj:-mllvm:--wasm-use-legacy-eh=false:-mllvm:--exception-model=wasm"
+
 python3 Tools/wasm/wasm_build.py wasix build -c
 
 rm -rf "artifacts/wasix-install" && mkdir -p "artifacts/wasix-install"
